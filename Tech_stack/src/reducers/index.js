2fa0bd66edcbdd6e4b14fe4ff98c008e928d6179
Jhentle Anamong <ajhentled@gmail.com>
2017-07-11 19:11:06 -0700

    Fix Tech_stack app file conflicts

--- Tech_stack/src/reducers/index.js
+++ Tech_stack/src/reducers/index.js
@@ -1,14 +1,8 @@
 import { combineReducers } from 'redux';
 import LibraryReducer from './LibraryReducer';
-<<<<<<< HEAD
 import SelectionReducer from './SelectionReducer';
 
 export default combineReducers({
   libraries: LibraryReducer,
   selectedLibraryId: SelectionReducer
-=======
-
-export default combineReducers({
-  libraries: LibraryReducer
->>>>>>> 60a83e9549319f322eaeb70bc810a2c43747c7fc
 });
